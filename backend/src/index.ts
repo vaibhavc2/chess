36dc194e70b30e7ded3cf8c5a1962d21180b7c37
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2025-03-22 02:04:00 +0530

    setup socketio

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
-import express from "express";
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
